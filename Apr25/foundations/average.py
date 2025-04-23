a696975ae0bf1b440fd2ac4a93f7b581d1a24142
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2025-04-23 08:28:07 +0530

    Added programs

---
+++ Apr25/foundations/average.py
@@ -0,0 +1,13 @@
+"""This program is to learn numeric types and its 
+operators
+
+Topics:
+  input
+  typecast
+"""
+
+maths = float(input("Enter maths marks: "))
+science = float(input("Enter science marks: "))
+social = float(input("Enter social marks: "))
+average = (maths + science + social) / 3
+print(average)
