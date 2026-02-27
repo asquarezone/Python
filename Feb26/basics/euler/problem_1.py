b40529fea971e01a7369ff3b071d5cc1075c1060
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-02-27 08:24:47 +0530

    Added problem 1 soluton

---
+++ Feb26/basics/euler/problem_1.py
@@ -0,0 +1,10 @@
+index = 1
+sum = 0
+max = 1000
+while index < max:
+    if index %3 == 0 or index % 5 == 0:
+        sum = sum + index
+    index = index + 1
+
+print("Result of project euler problem 1 is below")
+print(sum)
