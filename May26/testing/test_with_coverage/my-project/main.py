c83d26112d47b3d84dd18ba05e1f63100e28d14c
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-05-04 09:33:00 +0530

    Added pytest with coverage

---
+++ May26/testing/test_with_coverage/my-project/main.py
@@ -0,0 +1,6 @@
+def is_even(n: int) -> bool:
+    result = n%2
+    if result == 0:
+        return True
+    return False
+
---
+++ May26/testing/test_with_coverage/my-project/test_main.py
@@ -0,0 +1,5 @@
+from main import is_even
+
+def test_is_even():
+    #assert is_even(2) == True
+    assert is_even(3) == False
