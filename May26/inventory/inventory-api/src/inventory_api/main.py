33c50b25ec30d2fab0a0ae1586bba89753d8d900
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-05-17 10:19:26 +0530

    Added changes

--- May26/inventory/inventory-api/src/inventory_api/main.py
+++ May26/inventory/inventory-api/src/inventory_api/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
 
 app = FastAPI(title="Inventory API")
 
@@ -6,6 +12,34 @@ app = FastAPI(title="Inventory API")
 def health_check():
     return {"status": "ok"}
 
-@app.get("/items")
-def list_items():
-    return [{"id": 1, "name": "Widget", "quantity": 10}]
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    """This api will return user information
+    """
+    print(f"fetching informaton for user with id {user_id}")
+    return {
+        "id": user_id,
+        "name": "John Doe",
+        "email": "john.doe@example.com"
+    }
+
+@app.get("/products/")
+async def get_products(category:str, limit:int = 10):
+    return {
+        "category": category,
+        "limit": limit
+    }
+
+
+@app.get("/profile")
+async def profile(authorization: str = Header(), version: str = Header(default='v1')):
+    return { 
+        "recieved_token": authorization,
+        "version": version
+    }
+
+@app.post("/users")
+async def create_user(user: User) -> User:
+    return user
