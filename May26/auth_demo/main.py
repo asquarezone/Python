92d1f9b691dc57ae530bca027eb12cf08dbc377e
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-06-13 09:24:48 +0530

    Added basic authentication

--- May26/auth_demo/main.py
+++ May26/auth_demo/main.py
@@ -1,6 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException,status
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
 
 app = FastAPI(title="fastapi authentication demo")
+
+def verify_credentials(credentials: HTTPBasicCredentials):
+    username="admin"
+    password="admin@123"
+    if credentials.username == username and credentials.password == password:
+        # right Credentials
+        return {
+            "name": "khaja",
+            "email": "test@test.com",
+            "mobile": "99999999999",
+            "address": "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        }
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"}, # Required by 401 spec
+        )
+
+
 
 @app.get("/public")
 def get_general_info():
@@ -9,8 +31,9 @@ def get_general_info():
         "description": "learning to build apis"
     }
 
-@app.get("/me")
-def get_profile():
+@app.post("/me")
+def get_profile(username:str = Depends(verify_credentials)):
+
     return {
         "name": "khaja",
         "email": "test@test.com",
